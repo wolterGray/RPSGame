369ec5040940b6095b0085899ebc53fabf3f6e6b
Volodymyr Vorobiov <funnyshotgun@gmail.com>
2024-02-26 00:51:28 +0100

    - Created the basic structure and layout of the App component
    -Tailwind installed

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
 import React from "react";
 
 function App() {
-  return(
-	<>
-	App
-	</>
-  )
+  const [startGame, setStartGame] = React.useState(false);
+
+  return (
+    <div
+      style={{
+        backgroundImage:
+          "radial-gradient(circle, hsl(214, 47%, 23%), hsl(237, 49%, 15%))",
+      }}
+      className="w-full uppercase font-semibold select-none  h-screen p-10 box-border text-textColor "></div>
+  );
 }
 
 export default App;
