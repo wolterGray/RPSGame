da5f56d6a073dd7c5be2592236cdbe07406bf3af
Volodymyr Vorobiov <funnyshotgun@gmail.com>
2024-02-26 00:46:34 +0100

    Initial comit

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    colors: {
+      textColor:'#ffff',
+      scoreColor:'hsl(229, 25%, 31%)',
+      outlineColor:"hsl(217, 16%, 45%)"
+     },
+    extend: {},
+  },
+  plugins: [],
+}
+
