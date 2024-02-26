d74f45cfb4473e1b94b526b410e3f2c8a22ec5f9
Volodymyr Vorobiov <funnyshotgun@gmail.com>
2024-02-26 07:57:21 +0100

    - Created and stylized the Header component connected to the App

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Header from "./Header";
 
 function App() {
   const [startGame, setStartGame] = React.useState(false);
@@ -9,7 +10,11 @@ function App() {
         backgroundImage:
           "radial-gradient(circle, hsl(214, 47%, 23%), hsl(237, 49%, 15%))",
       }}
-      className="w-full uppercase font-semibold select-none  h-screen p-10 box-border text-textColor "></div>
+      className="w-full uppercase font-semibold select-none  h-screen p-10 box-border text-textColor ">
+			<div className="w-6/12 mx-auto grid grid-rows-[1fr,_5fr]">
+        <Header />
+      </div>
+		</div>
   );
 }
 
---
+++ src/Header.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+function Header() {
+  return (
+    <header className="flex justify-between items-center gap-10 border-2 h-full relative rounded-xl w-full p-4 border-solid border-outlineColor uppercase ">
+      <div className="">
+        <h1 className="leading-[0.8] flex flex-col font-semibold text-4xl">
+          <span>Rock</span>
+          <span>Paper</span>
+          <span>Scissors</span>
+        </h1>
+      </div>
+      <div className=" bg-[white] font-semibold rounded-xl flex flex-col items-center justify-center h-full w-2/12 text-scoreColor">
+        <p className="text-xs tracking-[.2em]">Score</p>
+        <p className="text-5xl">0</p>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
